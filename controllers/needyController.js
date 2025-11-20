fcb53622091f7b756107401977dad7839ea71560
Premal005 <premal.goyal@gmail.com>
2025-11-20 17:03:44 +0530

    gemini2

--- controllers/needyController.js
+++ controllers/needyController.js
@@ -66,7 +66,7 @@ const uploadBack = async (req, res) => {
     const savePath = path.join(outputFolder, "back_" + Date.now() + path.extname(file.name));
     await file.mv(savePath);
 
-    const model = genAI.getGenerativeModel({ model: "models/gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "models/gemini-2.5-flash" });
     const imageBytes = fs.readFileSync(savePath).toString("base64");
     const result = await model.generateContent([
       {
